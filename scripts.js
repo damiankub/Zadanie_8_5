c9ea4ee34cb5a91a7ed93568855b6694f1e68bd9
Damian <damiankub89@gmail.com>
2017-09-25 20:18:33 +0200

    Zadanie 8.5

---
+++ scripts.js
@@ -0,0 +1,7 @@
+
+var a = prompt('Podaj liczbe A :');
+var h = prompt('Podaj liczbe H :');
+var triangleArea = a*h/2;
+
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
